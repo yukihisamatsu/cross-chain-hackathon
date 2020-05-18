ab2122e13f5b97c98a06a26fd9f5f5d9ac5928be
toshihiko-okubo <toshihiko.okubo@speee.jp>
2020-05-18 19:46:34 +0900

    fix dividend components

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -13,5 +13,5 @@ yarn build:prod
 dirs=("./public" "./dist")
 
 for i in "${dirs[@]}" ; do
-    ${AWS} s3 cp "${i}" s3://"${S3_BUCKET}"/ --region ${AWS_REGION} --recursive --metadata-directive REPLACE --cache-control max-age=300
+    ${AWS} s3 cp "${i}" s3://"${S3_BUCKET}"/ --region ${AWS_REGION} --recursive --metadata-directive REPLACE --cache-control max-age=600
 done
